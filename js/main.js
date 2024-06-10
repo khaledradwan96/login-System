105914b99d12d18d8d029b744198c4e2695dd460
Khaled Radwan <khaledradwan96@gmail.com>
2024-06-11 02:28:55 +0300

    add code of functions => (checkEmpty + checkEmails + validEmail + signUp)

--- js/main.js
+++ js/main.js
@@ -5,27 +5,64 @@ var signupRePassword = document.getElementById("signupRePassword")
 var result = document.getElementById("result")
 var btn = document.querySelector('.btn')
 
+var signupData
+if(localStorage.getItem('users') === null){
+    signupData = [];
+}else{ // get signupData from localStorage
+    signupData = JSON.parse(localStorage.getItem('users'))
+}
+
 btn.addEventListener("click", function(e){
     e.preventDefault()
 })
 
+    // check all inputs have values or empty
+function checkEmpty(){
+    if(signupName.value == "" || signupEmail.value == "" || signupPassword.value == "" || signupRePassword.value == ""){
+            return false;
+    }
+}
+
+    // check email exist
+function checkEmails(){
+    for(var i=0; i < signupData.length; i++){
+        if(signupEmail.value === signupData[i].email){
+            return false;
+        }
+    }
+}
+
+    // Check validate emails
+function validEmail(){
+    var emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    if(emailRegex.test(signupEmail.value)){
+        return true;
+    }
+}
+
 btn.addEventListener("click", signUp)
-var signupData = []
-
-
-
-
-
 function signUp(){
+    if(checkEmails() == false){
+        result.innerHTML = '<span class="text-danger">email already exists</span>'
+    }
+
+    if(validEmail() != true){
+        result.innerHTML = '<span class="text-danger">email is not valid</span>'
+    }
+
+    if(checkEmpty() == false){
+        result.innerHTML = '<span class="text-danger">All inputs is required</span>'
+    }
+
+    // store data
     var signUp = {
         name : signupName.value,
         email: signupEmail.value,
         password: signupPassword.value
     }
-    // check all inputs have values or empty
-    if(signupName.value == "" || signupEmail.value == "" || signupPassword.value == "" || signupRePassword.value == ""){
-        result.innerHTML = '<span class="text-danger">All inputs is required</span>'
+    if(checkEmpty() != false && checkEmails() != false && validEmail() == true){
+        signupData.push(signUp)
+        localStorage.setItem('users', JSON.stringify(signupData))
+        result.innerHTML = '<span class="text-success">Sign Up Success</span>'
     }
-
-    console.log(signUp)
 }
