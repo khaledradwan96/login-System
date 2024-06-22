aaece01c6a25b61fab05d1b671909a355abe7a4e
Khaled Radwan <khaledradwan96@gmail.com>
2024-06-22 22:52:24 +0300

    reformating JS code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
+// var inputs
 var signupName = document.getElementById("signupName")
 var signupEmail = document.getElementById("signupEmail")
 var signupPassword = document.getElementById("signupPassword")
 var signupRePassword = document.getElementById("signupRePassword")
 var result = document.getElementById("result")
+
 var btn = document.querySelector('.btn')
 
+// signup Data Array
 var signupData
-if(localStorage.getItem('users') === null){
+if(localStorage.getItem('users') === null){ // new user
     signupData = [];
 }else{ // get signupData from localStorage
     signupData = JSON.parse(localStorage.getItem('users'))
@@ -16,14 +19,16 @@ btn.addEventListener("click", function(e){
     e.preventDefault()
 })
 
-    // check all inputs have values or empty
+// ============  Signup page (index) ============
+// to accept signup data we have to check three check
+    // 01 - check all inputs have values or empty
 function checkEmpty(){
     if(signupName.value == "" || signupEmail.value == "" || signupPassword.value == "" || signupRePassword.value == ""){
             return false;
     }
 }
 
-    // check email exist
+    // 02 - check email exist
 function checkEmails(){
     for(var i=0; i < signupData.length; i++){
         if(signupEmail.value === signupData[i].email){
@@ -32,7 +37,7 @@ function checkEmails(){
     }
 }
 
-    // Check validate emails
+    // 03- Check validate emails
 function validEmail(){
     var emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
     if(emailRegex.test(signupEmail.value)){
@@ -64,38 +69,34 @@ function signUp(){
         localStorage.setItem('users', JSON.stringify(signupData))
         result.innerHTML = '<span class="text-success">Sign Up Success</span>'
     }
-    
-console.log(signupData)
 }
 
 // ============  login page ============
 var loginEmail = document.getElementById("loginEmail")
 var loginPassword = document.getElementById("loginPassword")
 
-    // check all inputs have values or empty
-    function checkLoginEmpty(){
-        if(loginEmail.value == "" || loginPassword.value == ""){
-                return false;
-        }
+// check all inputs have values or empty
+function checkLoginEmpty(){
+    if(loginEmail.value == "" || loginPassword.value == ""){
+        return false;
     }
+}
 
 function login(){
     if(checkLoginEmpty() == false){
-        console.log("empty")
         result.innerHTML = '<span class="text-danger">All inputs is required</span>'
     }else{
-    var email = loginEmail.value
-    var password = loginPassword.value;
-    for(var i =0; i<signupData.length; i++){
-    if(signupData[i].email.toLowerCase() == email.toLowerCase() && signupData[i].password == password){
-        sessionStorage.setItem('userName', signupData[i].name)
-        console.log("welcome")
-        location.replace('home.html')
-    }else{
-        result.innerHTML = '<span class="text-danger">incorrect email or password</span>'
+        var email = loginEmail.value
+        var password = loginPassword.value;
+        for(var i =0; i<signupData.length; i++){
+            if(signupData[i].email.toLowerCase() == email.toLowerCase() && signupData[i].password == password){
+                sessionStorage.setItem('userName', signupData[i].name)
+                location.replace('home.html')
+            }else{
+                result.innerHTML = '<span class="text-danger">incorrect email or password</span>'
+            }
         }
     }
-}
 }
 
 // ============  Home page ============
@@ -103,7 +104,6 @@ var userName = sessionStorage.getItem('userName')
 if(userName){
     document.querySelector(".box p").innerHTML = "Welcome "+ userName
 }
-
 
 function logout(){
     sessionStorage.removeItem("userName")
